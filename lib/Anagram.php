2abd6c88ec247da1876a572c77554bd149927175
Stephen Hardisty <shardisty@jhart0506.win.int.realestate.com.au>
2010-05-25 16:29:32 +1000

    We made php awesomeness

--- lib/Anagram.php
+++ lib/Anagram.php
@@ -1,11 +1,32 @@
 <?php
 
 class Anagram {
+
+
     public function anagramFor($word) {
-        return array('oils', 'soil', 'soli');
+        $key = $this->keyFor($word);
+        $words = $this->anagramForKey($key);
+        $result = array_diff($words, array($word));
+        sort($result);
+        return $result;
     }
 
+    private function keyFor($word) {
+        $result = str_split(trim($word));
+        sort($result);
+        return join('', $result);
+    }
+    
+    private function anagramForKey($inputKey) {
+        $dictWordsWithNewLines = file("/usr/share/dict/words");
+        $result = array();
+        foreach ($dictWordsWithNewLines as $word) {
+            $key = $this->keyFor($word);
+            if ($key == $inputKey)
+                $result[] = trim($word);
+        }
+        return $result;
+    }
 }
 
 ?>
-
--- test/AnagramTest.php
+++ test/AnagramTest.php
@@ -7,8 +7,15 @@ class AnagramTest extends PHPUnit_Framework_TestCase
     public function testShouldGetAnagramsForSilo()
     {
         $anagram = new Anagram();
-        $this->assertEquals($anagram->anagramFor('silo'), array('oils', 'soil', 'soli'));
+        $this->assertEquals($anagram->anagramFor('silo'), array('siol', 'soil', 'soli'));
     }
+
+    public function testShouldGetAnagramsForOils()
+    {
+        $anagram = new Anagram();
+        $this->assertEquals($anagram->anagramFor('oils'), array('silo', 'siol', 'soil', 'soli'));
+    }
+
 }
 
 ?>
